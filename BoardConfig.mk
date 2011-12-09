9d4812d425e3fd376faa88445bfae3add737038f
Wang Guojian <41245110@qq.com>
2011-12-09 17:49:29 +0800

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/motorola/spyder/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := omap4
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := spyder
+
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+#0x00380000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 9437184
+#0x00480000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/motorola/spyder/kernel
+
+#BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+#BOARD_HAS_LARGE_FILESYSTEM := true
